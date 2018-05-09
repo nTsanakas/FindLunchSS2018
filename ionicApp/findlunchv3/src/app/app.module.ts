aca91c31e2912b0cbbd16a3e2ed69c21ac7172be
AndreasGeyer <andreas.geyer@geyer-net.de>
2018-05-09 14:47:15 +0200

    changed Server Adress to Local IP

--- ionicApp/findlunchv3/src/app/app.module.ts
+++ ionicApp/findlunchv3/src/app/app.module.ts
@@ -67,7 +67,7 @@ import {Network} from "@ionic-native/network";
 import {ChooseLoginPage} from "../pages/choose-login/choose-login";
 import {FCM} from "@ionic-native/fcm";
 
-export const SERVER_URL: string = "https://demo.findlunch.de";
+export const SERVER_URL: string = "http://192.168.2.118:8080";
 export const APP_LANG: string = "de";
 export const FCM_SENDER_ID: string = '343682752512';
 
