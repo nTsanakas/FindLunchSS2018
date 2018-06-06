fda8fc23f5923b13d80ca5997be03cd3eb3c13cd
ChristianKeck <christian.keck@hm.edu>
2018-06-06 16:30:52 +0200

    REST-Endpunkt für Aktivieren/Deaktivieren von Push-Benachrichtigungen geändert.
    Signed-off-by: ChristianKeck <christian.keck@hm.edu>

--- Webapp/src/main/java/edu/hm/cs/projektstudium/findlunch/webapp/controller/rest/UserRestController.java
+++ Webapp/src/main/java/edu/hm/cs/projektstudium/findlunch/webapp/controller/rest/UserRestController.java
@@ -3,7 +3,6 @@ package edu.hm.cs.projektstudium.findlunch.webapp.controller.rest;
 import edu.hm.cs.projektstudium.findlunch.webapp.logging.LogUtils;
 import edu.hm.cs.projektstudium.findlunch.webapp.model.User;
 import edu.hm.cs.projektstudium.findlunch.webapp.repositories.UserRepository;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
@@ -13,7 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -29,17 +31,19 @@ import java.security.Principal;
 public class UserRestController {
 
     /** The user repository. */
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     /** The logger. */
-    private final Logger LOGGER = LoggerFactory.getLogger(PushNotificationRestController.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
+
+    @Autowired
+    public UserRestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     /**
-     * REST-Endpunkt zum Ändern eines Attributs des Users.
+     * REST-Endpunkt zum Aktivieren der Push-Notifikationen des Users.
      *
-     * @param id            Id des Users, bei dem ein Attribut geändert werden soll.
-     * @param enabled       Push-Nachrichten aktiviert oder deaktiviert.
      * @param principal     Principal des eingeloggten Users.
      * @param request       HTTP-Request
      * @return              Response mit Statuscode.
@@ -47,32 +51,56 @@ public class UserRestController {
     @CrossOrigin
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(
-            path = "/api/users/{id}/pushNotifications",
-            method = RequestMethod.PATCH)
+            path = "/api/pushNotifications",
+            method = RequestMethod.PUT)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Push-Notifikation erfolgreich aktiviert/deaktiviert."),
+            @ApiResponse(code = 200, message = "Push-Notifikation erfolgreich aktiviert."),
             @ApiResponse(code = 401, message = "User nicht gefunden")})
-    public ResponseEntity<String> changePushSubscription(
-            @PathVariable int id,
-            @RequestParam(name = "enabled")
-            @ApiParam(
-                    name = "enabled",
-                    value = "aktiviert",
-                    required = true)
-                    Boolean enabled,
+    public ResponseEntity<String> activatePushNotification(
             HttpServletRequest request,
             Principal principal) {
-
         LOGGER.info(LogUtils.getInfoStringWithParameterList(request, Thread.currentThread().getStackTrace()[1].getMethodName()));
+        return changePushNotification(principal, true);
+    }
 
+    /**
+     * REST-Endpunkt zum Deativieren der Push-Notifikationen des Users.
+     *
+     * @param principal     Principal des eingeloggten Users.
+     * @param request       HTTP-Request
+     * @return              Response mit Statuscode.
+     */
+    @CrossOrigin
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(
+            path = "/api/pushNotifications",
+            method = RequestMethod.DELETE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Push-Notifikation erfolgreich deaktiviert."),
+            @ApiResponse(code = 401, message = "User nicht gefunden")})
+    public ResponseEntity<String> deactivatePushNotification(
+            HttpServletRequest request,
+            Principal principal) {
+        LOGGER.info(LogUtils.getInfoStringWithParameterList(request, Thread.currentThread().getStackTrace()[1].getMethodName()));
+        return changePushNotification(principal, false);
+    }
+
+    /**
+     * Methode zum Ändern der Push-Benachrichtigungserlaubnis.
+     *
+     * @param principal Principal des eingeloggten Users
+     * @param enabled aktivieren oder deaktivieren
+     * @return ResponseEntity mit Ergebnis der Änderung
+     */
+    private ResponseEntity<String> changePushNotification(Principal principal, boolean enabled) {
         // User anhand ID finden.
-        User user = userRepository.findOne(id);
-        if(user != null) {
+        User user = userRepository.getOne(((User) ((Authentication) principal).getPrincipal()).getId());
+        if(user!=null) {
             // Wenn User zur ID vorhanden ist, prüfen, ob ID der des anfragenden Users entspricht.
-            if (id == ((User) ((Authentication) principal).getPrincipal()).getId()){
+            if (user.getId()>0){
                 user.setPushNotificationEnabled(enabled);
                 userRepository.save(user);
-                return new ResponseEntity<>("push-notifications enabled: " + enabled, HttpStatus.OK);
+                return new ResponseEntity<>("push-notifications enabled: " + user.isPushNotificationEnabled(), HttpStatus.OK);
             }
         }
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
